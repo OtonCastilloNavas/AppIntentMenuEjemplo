2fbb5b4d9c5664e010f789cbe90cdbab25732099
otoncn <omegasindrome>
2019-04-10 16:39:22 -0600

    layout adapter

--- app/src/main/java/com/cam/appintentmenu/RegistroActivity.java
+++ app/src/main/java/com/cam/appintentmenu/RegistroActivity.java
@@ -24,7 +24,7 @@ public class RegistroActivity extends AppCompatActivity {
         Spinner spNombres= findViewById(R.id.spNombres);
         adapter =
                 new ArrayAdapter<String>(this,
-                        android.R.layout.simple_spinner_item,
+                        android.R.layout.simple_spinner_dropdown_item,
                         nombreList);
         spNombres.setAdapter(adapter);
     }
